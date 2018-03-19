b5daeea908ea5a34dfd9ba8b339871ed04e75a4b
M. Alonso <port02.server@gmail.com>
2018-03-19 11:22:47 +0000

    update version

--- configuration.py
+++ configuration.py
@@ -1,5 +1,24 @@
-# This file is part of Tryton.  The COPYRIGHT file at the top level of
-# this repository contains the full copyright notices and license terms.
+##############################################################################
+#
+#    GNU Condo: The Free Management Condominium System
+#    Copyright (C) 2016- M. Alonso <port02.server@gmail.com>
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 from trytond.model import ModelView, ModelSQL, ModelSingleton, fields
 
 try:
@@ -10,16 +29,17 @@ except ImportError:
 TIMEZONES += [(None, '')]
 
 
-__all__ = ['Configuration']
+__all__ = ['Configuration',
+            ]
 
 
 class Configuration(ModelSingleton, ModelSQL, ModelView):
-    'Company Configuration'
+    "Company Configuration"
     __name__ = 'company.configuration'
 
     company_currency = fields.Property(fields.Many2One('currency.currency',
-            'Company Currency', help=('The value set on this field will preset the currency on new '
-            'companies')))
+        'Company Currency', help=('The value set on this field will preset the currency on new '
+        'companies')))
     company_timezone = fields.Property(fields.Selection(TIMEZONES, 'Company Timezone', translate=False,
         help=('The value set on this field will preset the timezone on new '
-            'companies')))
+        'companies')))
